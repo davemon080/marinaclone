8d0222c0d5081472d6a487dbceca66e77907aa16
davemon080 <davemon080@gmail.com>
2026-08-17 18:30:43 +0100

    error: 'Document not found', removed

--- api/verify-certificate.js
+++ api/verify-certificate.js
@@ -386,7 +386,7 @@ export default async function handler(req, res) {
       ok: false,
       data: {},
       message: 'Document not found',
-      error: 'Document not found',
+     
     });
   } catch (globalError) {
     console.error('[Unhandled API Error in verify-certificate]', globalError);
